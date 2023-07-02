d34def564e815933235349766e6a1dc733332436
Mario Vasilev <mariovasilev212@gmail.com>
2023-07-02 11:16:25 +0100

    add function to compute moving averages over last k time steps

--- graph_irl/vis_utils.py
+++ graph_irl/vis_utils.py
@@ -1,5 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pickle
+from pathlib import Path
+
+
+def get_moving_avgs(returns_over_episodes, num_steps):
+    """
+    Return len(returns_over_episodes) - num_steps + 1 moving
+    averages over num_steps steps.
+    """
+    if num_steps > len(returns_over_episodes):
+        raise ValueError('num_steps should be less than' 
+                ' or equal to length of returns_over_episodes')
+    return np.correlate(returns_over_episodes, 
+            np.ones(num_steps), mode='valid') / num_steps
 
 
 def ci_plot(ax, data: np.ndarray, **kwargs):
@@ -11,3 +25,4 @@ def ci_plot(ax, data: np.ndarray, **kwargs):
     sd = np.std(data, 0)
     ax.fill_between(x, avg - sd, avg + sd, alpha=0.2, **kwargs)
     ax.plot(x, avg, **kwargs)
+
