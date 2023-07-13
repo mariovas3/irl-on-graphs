2829eda2cb031beb4d897b1de804a76dca552683
Mario Vasilev <mariovasilev212@gmail.com>
2023-07-13 12:05:47 +0100

    black formatting for graph_rl_utils

--- graph_irl/graph_rl_utils.py
+++ graph_irl/graph_rl_utils.py
@@ -6,23 +6,27 @@ from torch_geometric.data import Data
 
 
 class GraphEnv:
-    def __init__(self, x: torch.Tensor,
-                 reward_fn: Callable, 
-                 max_episode_steps: int, 
-                 num_expert_steps: int,
-                 max_repeats:int):
+    def __init__(
+        self,
+        x: torch.Tensor,
+        reward_fn: Callable,
+        max_episode_steps: int,
+        num_expert_steps: int,
+        max_repeats: int,
+    ):
         """
         Args:
             x (torch.Tensor): Initial node features.
-            reward_fn (Callable): Accept a graph state summary and 
+            reward_fn (Callable): Accept a graph state summary and
                 and output a scalar reward.
             max_episode_steps (int): Truncation length.
             num_expert_steps (int): Termination length.
             max_repeats (int): Truncate if add >= max_repeats existing edges.
         """
-        self.x = x 
-        self.data = Data(x=x, 
-                         edge_index=torch.tensor([[], []], dtype=torch.long))
+        self.x = x
+        self.data = Data(
+            x=x, edge_index=torch.tensor([[], []], dtype=torch.long)
+        )
         self.unique_edges = set()
         self.max_episode_steps = max_episode_steps
         self.num_expert_steps = num_expert_steps
@@ -31,8 +35,11 @@ class GraphEnv:
         self.repeats_done = 0
         self.terminated, self.truncated = False, False
         self.num_self_loops = 0
+        # reward fn should have its own GNN encoder;
+        # due to deterministic transitions, make reward
+        # state dependent only, since S x A -> S deterministically.
         self.reward_fn = reward_fn
-    
+
     def reset(self):
         """Returns (observation, None) to be compatible with openai gym."""
         self.data.edge_index = torch.tensor([[], []], dtype=torch.long)
@@ -42,28 +49,28 @@ class GraphEnv:
         self.terminated, self.truncated = False, False
         self.num_self_loops = 0
         return self.data, None
-    
-    def step(self, a: np.ndarray, x_embeds: np.ndarray):
+
+    def step(self, action):
         """Returns (observation, terminated, truncated, None)."""
         assert not (self.terminated or self.truncated)
         info = {
-            'terminated': False,
-            'expert_episode_length_reached': False,
-            'truncated': False,
-            'max_repeats_reached': False,
-            'episode_truncation_length_reached': False,
-            'self_loop': False,
-            'steps_done': self.steps_done,
-            'repeats_done': self.repeats_done,
-            'num_self_loops': self.num_self_loops
+            "terminated": False,
+            "expert_episode_length_reached": False,
+            "truncated": False,
+            "max_repeats_reached": False,
+            "episode_truncation_length_reached": False,
+            "self_loop": False,
+            "steps_done": self.steps_done,
+            "repeats_done": self.repeats_done,
+            "num_self_loops": self.num_self_loops,
         }
-
+        a, x_embeds = action
         tree = KDTree(x_embeds)
         obs_dim = x_embeds.shape[-1]
         action_dim = len(a)
-        
+
         # graph opt like edge addition;
-        # connects point closes to a1 to point 
+        # connects point closes to a1 to point
         # closest to a2, these could be very far apart.
         # also not clear to me how NN will figure out
         # which ones to connect since they share a common embedding;
@@ -80,52 +87,68 @@ class GraphEnv:
         else:
             idxs = tree.query(a, k=2)[-1]
             first, second = idxs[0], idxs[1]
-        
+
         if first == second:
             self.num_self_loops += 1
-            info['self_loop'] = True
-            info['repeats_done'] = self.repeats_done
-            info['steps_done'] = self.steps_done
-            info['num_self_loops'] = self.num_self_loops
+            info["self_loop"] = True
+            info["repeats_done"] = self.repeats_done
+            info["steps_done"] = self.steps_done
+            info["num_self_loops"] = self.num_self_loops
             return self.data, self.reward_fn(self.data), False, False, info
-        
-        if (first, second) in self.unique_edges or (second, first) in self.unique_edges:
+
+        if (first, second) in self.unique_edges or (
+            second,
+            first,
+        ) in self.unique_edges:
             self.repeats_done += 1
             if self.repeats_done >= self.max_repeats:
-                info['max_repeats_reached'] = True
-                info['truncated'] = True
-                info['repeats_done'] = self.repeats_done
-                info['steps_done'] = self.steps_done
-                info['num_self_loops'] = self.num_self_loops
+                info["max_repeats_reached"] = True
+                info["truncated"] = True
+                info["repeats_done"] = self.repeats_done
+                info["steps_done"] = self.steps_done
+                info["num_self_loops"] = self.num_self_loops
                 return self.data, self.reward_fn(self.data), False, True, info
         else:
             # add undirected edge to the graph Data container;
             self.data.edge_index = torch.cat(
-                    (
-                        self.data.edge_index, 
-                        torch.tensor([(first, second), (second, first)], 
-                                    dtype=torch.long)
-                    ), -1
-                )
-            
+                (
+                    self.data.edge_index,
+                    torch.tensor(
+                        [(first, second), (second, first)], dtype=torch.long
+                    ),
+                ),
+                -1,
+            )
+
             # increment steps done;
             self.steps_done += 1
 
             # add edge to set of edges;
             self.unique_edges.add((first, second))
             self.unique_edges.add((second, first))
-        
+
         # calculate outcomes;
         terminated = self.steps_done >= self.num_expert_steps
-        truncated = self.steps_done >= self.max_episode_steps or self.repeats_done >= self.max_repeats
-        info['terminated'] = terminated
-        info['truncated'] = truncated
-        info['episode_truncation_length_reached'] = self.steps_done >= self.max_episode_steps
-        info['max_repeats_reached'] = self.repeats_done >= self.max_repeats
-        info['expert_episode_length_reached'] = terminated
-        info['steps_done'] = self.steps_done
-        info['repeats_done'] = self.repeats_done
-        info['num_self_loops'] = self.num_self_loops
+        truncated = (
+            self.steps_done >= self.max_episode_steps
+            or self.repeats_done >= self.max_repeats
+        )
+        info["terminated"] = terminated
+        info["truncated"] = truncated
+        info["episode_truncation_length_reached"] = (
+            self.steps_done >= self.max_episode_steps
+        )
+        info["max_repeats_reached"] = self.repeats_done >= self.max_repeats
+        info["expert_episode_length_reached"] = terminated
+        info["steps_done"] = self.steps_done
+        info["repeats_done"] = self.repeats_done
+        info["num_self_loops"] = self.num_self_loops
         self.terminated, self.truncated = terminated, truncated
         # reward fn should have its own encoder GNN;
-        return self.data, self.reward_fn(self.data), terminated, truncated, info
+        return (
+            self.data,
+            self.reward_fn(self.data),
+            terminated,
+            truncated,
+            info,
+        )
